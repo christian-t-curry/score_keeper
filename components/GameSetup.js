099220d0ba04b19e62e1a43ee82897348256dddd
Chris Curry <chriscurry1@gmail.com>
2018-04-02 23:45:24 -0500

    working on text input

--- components/GameSetup.js
+++ components/GameSetup.js
@@ -1,37 +1,83 @@
+/*
+NOTES:
+--I should move the state of player one and player two out of this component and instead pass them straight
+through from the view controller into a text input component.
+*/
+
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, TextInput , KeyboardAvoidingView, Alert } from 'react-native';
 
 export default class GameSetup extends React.Component {
+
+  constructor(props) {
+    super (props);
+    this.state = {
+      playerOne: this.props.playerOne,
+    };
+  }
+
   handlePress() {
     this.props.handlePress();
   }
 
+  handleBlur(doThis, player) {
+    doThis(player);
+  }
+
   render() {
     return(
-      <View style={styles.gameSetupWrapper}>
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={styles.gameSetupWrapper}>
+
         <View style={styles.inputSection}>
+
+          //Player One Input
           <View style={styles.inputSectionRow} >
             <Text style={styles.inputLabel}>
               Player one
             </Text>
           </View>
+
           <View style={styles.inputSectionRow} >
             <TextInput
-              placeholder={this.props.playerOne}
+              value={this.state.playerOne}
+              style={styles.textInput}
+              onChangeText={(playerOne) => this.setState({playerOne})}
+              maxLength={35}
+              onBlur={this.handleBlur.bind(this, this.props.updatePlayerOne, this.state.playerOne)}
               ></TextInput>
           </View>
+
+          //Player Two Input
+          <View style={styles.inputSectionRow} >
+            <Text style={styles.inputLabel}>
+              Player Two
+            </Text>
+          </View>
+
+          <View style={styles.inputSectionRow} >
+            <TextInput
+              value={this.props.playerTwo}
+              style={styles.textInput}
+              maxLength={35}
+              ></TextInput>
+          </View>
+
+          //Start Game Button
           <View style={styles.inputSectionRow} >
             <TouchableOpacity
               style={styles.primaryButton}
               onPress={this.handlePress.bind(this)}
               >
-              <Text>
+              <Text style={styles.primaryButtonText}>
                 Start Game
               </Text>
             </TouchableOpacity>
           </View>
+
         </View>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -41,17 +87,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'pink',
   },
   primaryButton: {
     flex: 1,
     height: 45,
-    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 4,
+    borderWidth: 1,
+    backgroundColor: 'black',
+  },
+  primaryButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
   },
   inputSection: {
-    backgroundColor: '#eaeaea',
     marginLeft: 20,
     marginRight: 20,
     justifyContent: 'center',
@@ -65,6 +116,15 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'left',
-    paddingBottom: 10,
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    height: 45,
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 20,
+    paddingLeft: 10,
+    fontSize: 16,
   }
 });
